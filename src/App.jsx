9a6cd95017ac476ff128f4c6776464b2e85fe74b
Syed Amaan Ali <amaan7733@Syeds-MacBook-Air.local>
2025-04-04 02:24:41 +0530

    initial commit post app installation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+
+  return (
+    <>
+<Button className='bg-blue-900'>Here</Button>
+    </>
+  );
+}
+
+export default App;
